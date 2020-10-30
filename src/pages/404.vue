9e67c2a000786ba9e93dc0cd28ca1c55dd1d7921
Marco Verheul <marco@corneelonline.nl>
2020-10-30 14:46:31 +0100

    Add basic 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <section class="page-intro">
+      <h1>Pagina niet gevonden</h1>
+    </section>
+    <section class="page-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404 Not Found'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

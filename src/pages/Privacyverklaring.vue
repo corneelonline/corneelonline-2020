4e7cf7b238a554edcb0c081e07b6ae677030f7ef
Marco Verheul <marco@corneelonline.nl>
2020-11-03 14:29:47 +0100

    Add default page

--- src/pages/Privacyverklaring.vue
+++ src/pages/Privacyverklaring.vue
@@ -1,14 +1,62 @@
 <template>
   <Layout>
-    <h1>Privacyverklaring</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <section class="page-intro">
+      <h1>{{ $page.content.title }}</h1>
+    </section>
+    <div class="page-content--default">
+      <PortableText :blocks="$page.content._rawBody" />
+    </div>
+    <ContactMe />
   </Layout>
 </template>
 
+<page-query>
+query {
+  content: sanityDefaultPage(id: "878c333b-3de8-414b-841b-2a3f1f5817fa") {
+    title
+    _rawBody
+    seo {
+      seo_title
+      meta_description
+    }
+  }
+}
+</page-query>
+
 <script>
+import PortableText from '~/components/sanity/PortableText.vue'
+import ContactMe from '~/components/layout/ContactMe.vue'
+
 export default {
-  metaInfo: {
-    title: 'Privacyverklaring'
+  metaInfo() {
+    return {
+      title: this.$page.content.seo_title,
+      meta: [
+        {
+          name: "description",
+          content: this.$page.content.seo_description
+        },
+        {
+          property: "og:title",
+          content: this.$page.content.seo_title
+        },
+        {
+          property: "og:description",
+          content: this.$page.content.seo_description
+        },
+        {
+          property: "og:image",
+          content: ""
+        }
+      ],
+      bodyAttrs: {
+        class: "default-page"
+      }
+    }
+  },
+  components: {
+    PortableText,
+    ContactMe
   }
 }
 </script>

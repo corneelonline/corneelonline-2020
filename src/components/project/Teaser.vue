220c39ab0b85bd4e8503251d16fe257c568f2774
Marco Verheul <marco@corneelonline.nl>
2020-11-06 14:34:13 +0100

    Improve project teaser responsive styles

--- src/components/project/Teaser.vue
+++ src/components/project/Teaser.vue
@@ -29,10 +29,47 @@ export default {
   height: 143px;
   
   @media (min-width: $md) {
-    height: 183px;
+    height: 158px;
   }
   @media (min-width: $lg) {
-    height: 218px;
+    height: 202px;
+  }
+
+  .project-img {
+    display: block;
+    width: 204px;
+    height: 143px;
+    background-image: url('~@/assets/images/bg/laptop.svg');
+    background-position: 0 0;
+    background-repeat: no-repeat;
+    background-size: contain;
+    position: relative;
+    margin: 0 auto;
+    
+    @media (min-width: $md) {
+      width: 220px;
+      height: 158px;
+    }
+    @media (min-width: $lg) {
+      width: 285px;
+      height: 202px;
+    }
+
+    img {
+      position: absolute;
+      top: 12px;
+      left: 17px;
+      width: 83%;
+    
+      @media (min-width: $md) {
+        top: 13px;
+        left: 19px;
+      }
+      @media (min-width: $lg) {
+        top: 17px;
+        left: 24px;
+      }
+    }
   }
 }
 .project__teaser a {
@@ -64,41 +101,5 @@ export default {
       text-align: center;
       @include font-size(0.9)
     }
-}
-.project-img {
-  display: block;
-  width: 204px;
-  height: 143px;
-  background-image: url('~@/assets/images/bg/laptop.svg');
-  background-position: 0 0;
-  background-repeat: no-repeat;
-  background-size: contain;
-  position: relative;
-  margin: 0 auto;
-  
-  @media (min-width: $md) {
-    width: 261px;
-    height: 183px;
-  }
-  @media (min-width: $lg) {
-    width: 311px;
-    height: 218px;
-  }
-
-  img {
-    position: absolute;
-    top: 12px;
-    left: 17px;
-    width: 83%;
-  
-    @media (min-width: $md) {
-      top: 16px;
-      left: 22px;
-    }
-    @media (min-width: $lg) {
-      top: 18px;
-      left: 25px;
-    }
-  }
 }
 </style>
